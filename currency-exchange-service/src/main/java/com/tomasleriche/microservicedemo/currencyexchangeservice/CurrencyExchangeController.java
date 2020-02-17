109870ad8ff83de9f01b888b9b6b70b1988a1d19
theJames <james.newton@cognizant.com>
2020-02-17 17:30:02 -0600

    added config server, began currency exchange service

---
+++ currency-exchange-service/src/main/java/com/tomasleriche/microservicedemo/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,31 @@
+package com.tomasleriche.microservicedemo.currencyexchangeservice;
+
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    private ExchangeValueRepository exchangeValueRepository;
+    private Environment environment;
+
+    public CurrencyExchangeController(Environment environment, ExchangeValueRepository exchangeValueRepository) {
+        this.environment = environment;
+        this.exchangeValueRepository = exchangeValueRepository;
+    }
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
+
+
+
+        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        return exchangeValue;
+    }
+}
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10001, 'USD', 'INR', 65, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10002, 'FRA', 'INR', 60, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(10003, 'EUR', 'XRP', 665, 0);
